cf55528dbda46ef701fc8e7c3c68fc9a55e054ba
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-02-01 20:06:20 +0700

    feat : add a function to calculate each element in the same position from two arrays of integer. Assume both arrays are of the same length.

--- index.js
+++ index.js
@@ -44,3 +44,9 @@ console.log(concatenatingWord(["apple", "banana", "cherry", "date"]));
 const convertToArray = (text) => text.split(" ");
 
 console.log(convertToArray("Hello World"));
+
+// Write a function to calculate each element in the same position from two arrays of integer. Assume both arrays are of the same length.
+
+const calculateEachElement = (listA, listB) => listA.slice().map((data, index) => data + listB[index]);
+
+console.log(calculateEachElement([1, 2, 3], [3, 2, 1]));
