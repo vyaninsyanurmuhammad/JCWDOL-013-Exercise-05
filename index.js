04365c8208ed9fd2d7aa92c9e3152f494116ec6e
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-02-02 02:16:59 +0700

    feat : add a function to insert multiple given integer (not an array) to an array and have a maximum size input

--- index.js
+++ index.js
@@ -74,3 +74,15 @@ const removeAllOddArray = (list) => {
 }
 
 console.log(removeAllOddArray([1, 2, 3, 4, 5, 6]));
+
+/*
+
+Write a function to insert multiple given integer (not an array) to an array and have a maximum size input. The
+array can only have a maximum size from a given input. (if the maximum size of the given input is 5 than the
+array can only contain 5 elements).
+
+*/
+
+const insertToArrayByMaxSize = (max, ...number) => number.slice(0, max);
+
+console.log(insertToArrayByMaxSize(5, 5, 10, 24, 3, 6, 7, 8));
